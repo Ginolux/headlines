1eb79558311089d0e2689248b83c4d4c4e399082
gino <gino@gino.com>
2017-12-13 12:30:17 +0000

    add rss feed

--- headlines.py
+++ headlines.py
@@ -5,7 +5,8 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn': 'http://rss.cnn.com/rss/edition.rss',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640',
-             'jw': 'https://www.jw.org/en/news/rss/FullNewsRSS/feed.xml'}
+             'jw': 'https://www.jw.org/en/news/rss/FullNewsRSS/feed.xml',
+             'bloomberg': 'https://www.bloomberg.com/professional/feed/'}
 
 app = Flask(__name__)
 
@@ -13,7 +14,7 @@ app = Flask(__name__)
 @app.route("/<publication>")
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed['entries'])
+    return render_template("home.html", articles=feed['entries'], publication=publication)
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
